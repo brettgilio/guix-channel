d964946ee3f02de226becf69b28340ba3d6c71d8
Brett Gilio <brettg@posteo.net>
2019-05-31 13:44:09 -0500

    Update non-gnu/packages/coq-extend.scm: Bump proof-general

--- non-gnu/packages/coq-extend.scm
+++ non-gnu/packages/coq-extend.scm
@@ -12,6 +12,7 @@
   #:use-module (gnu packages python)
   #:use-module (gnu packages rsync)
   #:use-module (gnu packages texinfo)
+  #:use-module (gnu packages version-control)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system ocaml)
   #:use-module (guix download)
@@ -22,9 +23,9 @@
   #:use-module ((srfi srfi-1) #:hide (zip)))
 
 ;; Stop tracking tags, and track master instead
-(define-public proof-general
-  (let ((commit "946be87a944c9d8b850fdddb83d36e2ef9dad5c9")
-        (revision "3"))
+(define-public proof-general-master
+  (let ((commit "0058999ac42d7d1b3ee093aa5a4e8956d1eb8a9c")
+        (revision "4"))
     (package
      (name "proof-general")
      (version (git-version "4.4" revision commit))
@@ -35,7 +36,7 @@
                     (commit commit)))
               (sha256
                (base32
-                "1clk1r5h0bjshw45ywqqfswkh6xpa563q8c2cmhy2rhf3l0g2g22"))
+                "0q4qm0vi08wrl0wdlz5fhyfj8g4w8l1k5jyhfflig7pnjdw19lqa"))
               (file-name (git-file-name name version))))
      (build-system gnu-build-system)
      (native-inputs
@@ -60,5 +61,3 @@
 assistant to write formal mathematical proofs using a variety of theorem
 provers.")
      (license license:gpl2+))))
-
-
