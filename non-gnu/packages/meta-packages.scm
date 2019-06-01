d427d52cd61f21ad679e36ec90ab259d8ef9293f
Brett Gilio <brettg@posteo.net>
2019-05-31 23:56:48 -0500

    Update non-gnu/packages/meta-packages.scm: Bump to 4

--- non-gnu/packages/meta-packages.scm
+++ non-gnu/packages/meta-packages.scm
@@ -24,7 +24,7 @@
 (define %meta-base
   (package
     (name #f)
-    (version "3")
+    (version "4")
     (source #f)
     (build-system trivial-build-system)
     (arguments '(#:builder (begin (mkdir %output) #t)))
