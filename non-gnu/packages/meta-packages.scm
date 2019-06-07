4f4853f563a167cc23558d7c270506fa769d2408
Brett Gilio <brettg@posteo.net>
2019-06-06 19:36:13 -0500

    Update non-gnu/packages/meta-packages.scm: 14

--- non-gnu/packages/meta-packages.scm
+++ non-gnu/packages/meta-packages.scm
@@ -29,7 +29,7 @@
 (define %meta-base
   (package
     (name #f)
-    (version "13")
+    (version "14")
     (source #f)
     (build-system trivial-build-system)
     (arguments '(#:builder (begin (mkdir %output) #t)))
